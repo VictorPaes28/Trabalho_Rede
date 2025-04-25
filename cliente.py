689196202ab3704802ee7bbb9fbc61756e468693
VictorPaes28 <vplpc@cesar.school>
2025-04-25 09:14:32 -0300

    segunda entrega.
    entrega 2 do trabalho de redes

--- cliente.py
+++ cliente.py
@@ -5,11 +5,25 @@ cliente.connect(("localhost", 12345))
 
 modo = "lote"
 tamanho = "3"
-mensagem = f"modo:{modo},tamanho:{tamanho}"
+handshake = f"modo:{modo},tamanho:{tamanho}"
+cliente.send(handshake.encode())
+print(f"[HANDSHAKE ENVIADO]: {handshake}")
 
-cliente.send(mensagem.encode())
-resposta = cliente.recv(1024).decode()
+mensagem = input("Digite a mensagem para enviar ao servidor: ")
+tamanho_pacote = 3
 
-print("Resposta do servidor:", resposta)
+for i in range(0, len(mensagem), tamanho_pacote):
+    dados = mensagem[i:i+tamanho_pacote]
+    seq = i // tamanho_pacote
+    pacote = f"{seq}|{dados}"
+
+    cliente.send(pacote.encode())
+    print(f"[PACOTE ENVIADO]: {pacote}")
+
+    ack = cliente.recv(1024).decode()
+    print(f"[ACK RECEBIDO PELO CLIENTE]: {ack}\n")
+
+cliente.send("FIM".encode())
+print("[SINAL DE FIM ENVIADO]")
 
 cliente.close()
--- servidor.py
+++ servidor.py
@@ -4,16 +4,34 @@ servidor = socket.socket()
 servidor.bind(("localhost", 12345))
 servidor.listen(1)
 
-print("Servidor esperando conexão...")
+print("Servidor esperando conexão...\n")
 
 conexao, endereco = servidor.accept()
 print("Cliente conectado:", endereco)
 
-mensagem = conexao.recv(1024).decode()
-print("Mensagem recebida do cliente:", mensagem)
+handshake = conexao.recv(1024).decode()
+print("[HANDSHAKE RECEBIDO]:", handshake)
 
-resposta = "Servidor recebeu o modo e tamanho"
-conexao.send(resposta.encode())
+mensagem_completa = ""
+while True:
+    pacote = conexao.recv(1024).decode()
+    if pacote == "FIM":
+        print("[FIM da transmissão recebido]")
+        break
+
+    print(f"[PACOTE RECEBIDO]: {pacote}")
+    try:
+        seq, dados = pacote.split("|")
+        print(f"[META] Seq: {seq} | Dados: {dados}")
+        mensagem_completa += dados
+
+        ack = f"ACK {seq}"
+        conexao.send(ack.encode())
+        print(f"[ACK ENVIADO]: {ack}\n")
+    except ValueError:
+        print("[ERRO] Formato inválido do pacote.")
+
+print("\n[MENSAGEM FINAL RECEBIDA COMPLETA]:", mensagem_completa)
 
 conexao.close()
 servidor.close()
